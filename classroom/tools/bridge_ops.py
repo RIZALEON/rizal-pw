c81fa8afcb7bbdd033a60af3da2444bd58074bb6
ЯBOT <Tamangkapa@icloud.com>
2026-09-26 03:15:55 -0600

    classroom: thin bridge_ops re-export (ЯBAT)

---
+++ classroom/tools/bridge_ops.py
@@ -0,0 +1,3 @@
+"""Classroom bridge companion. Re-exports from bridge_ops_impl."""
+from bridge_ops_impl import *  # noqa: F401,F403
+from bridge_ops_impl import wire, bridge_cmd, validate_bridge_tree, validate_bridge_path_allowlist
